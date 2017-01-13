d269daec4861e8e2c7c9eddfd4a61d3fb13d8eb3
josizqnav <josizqnav123@gmail.com>
2017-01-13 11:23:40 +0100

    Configurados test.

--- child2/src/main/java/Example/App.java
+++ child2/src/main/java/Example/App.java
@@ -1,13 +1,9 @@
 package Example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Auto generated maven project" );
+        System.out.println( "Child 2" );
     }
 }
